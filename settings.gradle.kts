5ebe00a64e0e73fb546422a9e17f3250cdf683e1
Leif Lislegard <lislei@kartverket.no>
2024-11-06 18:40:17 +0100

    QA Ryddet ubrukt plugin `org.jetbrains.kotlin.kapt`.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -9,7 +9,6 @@ pluginManagement {
         id ("org.jetbrains.kotlin.jvm") version kotlinVersion
         id ("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
         id ("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
-        id ("org.jetbrains.kotlin.kapt") version kotlinVersion
     }
 }
 
