67899d81cb8e2b7069eae24c5a9ae56a14c7c293
Martin Halleland <martin.halleland@gmail.com>
2025-02-07 09:02:06 +0100

    TH-2019: Oppdaterer gradle wrapper til 8.12.1
    # Conflicts:
    #	mat-auth-ham/src/main/kotlin/no/statkart/matrikkel/auth/ham/AbstractDefaultHttpAuthenticationMechanism.kt

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,7 +5,7 @@ pluginManagement {
     }
 
     plugins {
-        val kotlinVersion = "1.4.21"
+        val kotlinVersion = "1.8.22"
         id ("org.jetbrains.kotlin.jvm") version kotlinVersion
         id ("org.jetbrains.kotlin.plugin.allopen") version kotlinVersion
         id ("org.jetbrains.kotlin.plugin.noarg") version kotlinVersion
