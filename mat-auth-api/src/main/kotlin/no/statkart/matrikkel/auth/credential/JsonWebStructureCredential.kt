71d6abd2af20b4b4380774a21cf36d1543017b33
Nicklas Utgaard <nutgaard@gmail.com>
2026-05-19 10:41:07 +0200

    TH-2510: plukke brukernavn/orgnr fra header når man prosesser bearer-auth
    `X-Matrikkel-Brukernavn` er brukt fordi dette er samme som brukes av bygning

--- mat-auth-api/src/main/kotlin/no/statkart/matrikkel/auth/credential/JsonWebStructureCredential.kt
+++ mat-auth-api/src/main/kotlin/no/statkart/matrikkel/auth/credential/JsonWebStructureCredential.kt
@@ -2,5 +2,8 @@ package no.statkart.matrikkel.auth.credential
 
 import jakarta.security.enterprise.credential.Credential
 
-data class JsonWebStructureCredential(val compactSerialization: String, val fromPasswordGrant: Boolean) : Credential {
-}
+data class JsonWebStructureCredential(
+    val compactSerialization: String,
+    val fromPasswordGrant: Boolean,
+    val username: String? = null
+) : Credential
