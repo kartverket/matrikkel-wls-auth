7c361c461d086188a79a3978ec4ae0e22cfeffc5
Nicklas Utgaard <nutgaard@gmail.com>
2026-05-19 10:41:07 +0200

    TH-2510: plukke brukernavn/orgnr fra header når man prosesser bearer-auth
    `X-Matrikkel-Brukernavn` er brukt fordi dette er samme som brukes av bygning

--- mat-auth-id-store-jwt/src/main/kotlin/no/statkart/matrikkel/auth/identity/jwt/BearerJWTCredentialExtractor.kt
+++ mat-auth-id-store-jwt/src/main/kotlin/no/statkart/matrikkel/auth/identity/jwt/BearerJWTCredentialExtractor.kt
@@ -7,13 +7,19 @@ import jakarta.servlet.http.HttpServletRequest
 
 @ApplicationScoped
 class BearerJWTCredentialExtractor : HttpCredentialExtractor<JsonWebStructureCredential> {
-    override fun getCredential(request: HttpServletRequest, map: Map<*, *>): JsonWebStructureCredential? =
-        request.getHeader("Authorization")?.let { credentials(it) }?.let {
-            JsonWebStructureCredential(it, false)
-        }
+    override fun getCredential(request: HttpServletRequest, map: Map<*, *>): JsonWebStructureCredential? {
+        val username: String? = request.getHeader("X-Matrikkel-Brukernavn")
+        val jwt: String = parse(request.getHeader("Authorization")) ?: return null
+
+        return JsonWebStructureCredential(
+            compactSerialization = jwt,
+            fromPasswordGrant = false,
+            username = username
+        )
+    }
 
     companion object {
-        private fun credentials(s: String?): String? {
+        private fun parse(s: String?): String? {
             if (s == null || s.length <= 10) return null
             return if (!s.substring(0, 7).equals("Bearer ", ignoreCase = true)) {
                 null
